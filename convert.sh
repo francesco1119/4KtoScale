5a7a460f70b4b0b8a5ebb1ca9d43d3a9d4fe270e
Francesco Mantovani <francesco84mantovani@gmail.com>
2017-02-20 22:16:10 +1300

    Delete convert.sh

--- convert.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh --
-INPUT="$1"
-mkdir output
-for i in *.mp4 ; do
-    ffmpeg -i "$i" -vf scale=1920:1080 -c:a copy -c:v libx264 -crf 20 -strict -2 -preset slow output/"$i"
-    
-    sleep 3
-done
-
